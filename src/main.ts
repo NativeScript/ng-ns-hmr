a6c88be37f796527f9cc498b5a33e2fb24e74d46
Vasil Chimev <vasil.chimev@gmail.com>
2019-02-12 14:56:16 +0200

    chore: update to {N} 5.2

--- src/main.ts
+++ src/main.ts
@@ -8,22 +8,25 @@ import "./livesync-navigation"
 
 let options: AppOptions = {};
 
-if (module["hot"]) {
-    options.hmrOptions = {
-        moduleTypeFactory: () => AppModule,
-        livesyncCallback: (platformReboot) => {
-            setTimeout(platformReboot, 0);
-        },
-    }
+// No need to manually handle changes anymore,
+// HMR is enabled by default in {N}-Angular projects
+// https://github.com/NativeScript/nativescript-dev-webpack/blob/master/CHANGELOG.md#breaking-changes
+// if (module["hot"]) {
+//     options.hmrOptions = {
+//         moduleTypeFactory: () => AppModule,
+//         livesyncCallback: (platformReboot) => {
+//             setTimeout(platformReboot, 0);
+//         },
+//     }
 
-    // Path to your app module.
-    // You might have to change it if your module is in a different place.
-    module["hot"].accept(["./app/app.module"], () => {
-        // Currently the context is needed only for application style modules.
-        const moduleContext = {};
-        global["hmrRefresh"](moduleContext);
-    });
-}
+//     // Path to your app module.
+//     // You might have to change it if your module is in a different place.
+//     module["hot"].accept(["./app/app.module"], () => {
+//         // Currently the context is needed only for application style modules.
+//         const moduleContext = {};
+//         global["hmrRefresh"](moduleContext);
+//     });
+// }
 
 // Don't forget to pass the options when creating the platform
 platformNativeScriptDynamic(options).bootstrapModule(AppModule);
